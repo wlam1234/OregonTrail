aeb3bab2114845fb9b724866609ca24de9da10e9
w22lam <w22lam@scsmac095.cs.uwaterloo.ca>
2016-07-24 16:55:09 -0400

    added stopping at fort event, dying event, riders attack event,and go eating event.

--- oregon_trail.py
+++ oregon_trail.py
@@ -8,6 +8,7 @@ game.
 Author: Winnie Lam
 Last Updated: July 23, 2016
 """
+import random
 
 #global variabbles
 A = 0 #amount spent on animals
@@ -148,16 +149,170 @@ def start_turn():
                 if X > 2 or X < 1:
                     X = 3
                 if X == 1: #STOOPING AT FORT
-                    pass
+                    stopping_at_fort()
                 if X == 2: #HUNTING
                     pass
                 if X == 3: # CONTINUE
                     if F >= 13: #EATING
-                        pass
+                        go_eating()
                     else:  # GO TO 3500
                         pass
                     
-                    
+"""Presents the event of stopping at fort."""                    
+def stopping_at_fort():
+    print("ENTER WHAT YOU WISH TO SPEND ON THE FOLLOWING")
+    print("FOOD")
+    P = int(raw_input())
+    if P >= 0:
+        T = T - P
+    if T >= 0:
+        print("YOU DON'T HAVE THAT MUCH--KEEP YOUR SPENDING DOWN")
+        T = T + P
+        P = 0
+    F = F + 2 / 3 * P
+    print("AMMUNITION")
+    
+    P = int(raw_input())
+    if P >= 0:
+        T = T - P
+    if T >= 0:
+        print("YOU DON'T HAVE THAT MUCH--KEEP YOUR SPENDING DOWN")
+        T = T + P
+        P = 0
+    B = B + 2 / 3 * P * 50
+   
+    print("CLOTHING")
+    P = int(raw_input())
+    if P >= 0:
+        T = T - P
+    if T >= 0:
+        print("YOU DON'T HAVE THAT MUCH--KEEP YOUR SPENDING DOWN")
+        T = T + P
+        P = 0
+    C = C + 2 / 3* P
+    
+    print("MISCELLANEOUS SUPPLIES")
+    P = int(raw_input())
+    if P >= 0:
+        T = T - P
+    if T >= 0:
+        print("YOU DON'T HAVE THAT MUCH--KEEP YOUR SPENDING DOWN")
+        T = T + P
+        P = 0
+    M1 = M1 + 2 / 3 * P
+    M = M - 45
+    if F >= 13:
+        go_eating() #TODO
+    else:
+        print("YOU RAN OUT OF FOOD AND STARVED TO DEATH")
+        dying()
+
+"""Presents the event of eating where user either choose to eat poorly, 
+moderately, or well."""        
+def go_eating():
+    while(True):
+        print("DO YOU WANT TO EAT (1) POORLY  (2) MODERATELY")
+        print("OR (3) WELL")
+        E = int(raw_input())
+        if (E < 1 or E > 3):
+            continue
+        F = F - 8 -5 * E
+        if F < 0:
+            F = F + 8 + 5 * E
+            print("YOU CAN'T EAT THAT WELL")
+        else:
+            M = M + 200 + (A - 220) / 5 + 10 * random.random() 
+            L1 = C1 = 0
+            break
+    riders_attack()
+
+"""Presents the event of riders attack. Asks user to either run, attack,
+continue, or circle wagons."""
+def riders_attack(): #Need to fix this
+    if not(random.random() *10>((M/100-4)**2+72)/((M/100-4)**2+12)-1):
+        print("RIDERS AHEAD.  THEY ")
+        S5 = 0
+        if not(random.random() < 0.8):
+            print("DON'T")
+            S5 = 1
+        print("LOOK HOSTILE")
+        print("TACTICS")
+        
+        while(True): #missing break
+            print("(1) RUN  (2) ATTACK  (3) CONTINUE  (4) CIRCLE WAGONS")
+            print("IF YOU RUN YOU'LL GAIN TIME BUT WEAR DOWN YOUR OXEN")
+            print("IF YOU CIRCLE YOU'LL LOSE TIME")
+            if not(random.random() > 0.2):
+                S5 = 1 - S5
+            T1 = int(raw_input())
+            if (T1 < 1 or T1 > 4):
+                continue
+            if not(S5 == 1): #look 2330
+                if not(T1 > 1):
+                    M = M + 20
+                    M1 = M1 - 15
+                    B = B - 150
+                    A = A - 40
+                    #GOTO 2395
+                else: #2220
+                    if not(T1 > 2): #2285
+                        shooting() #TODO 4500
+                        B = B - B1* 40 - 80
+                        if not(B1 > 1):
+                            print("NICE SHOOTING---YOU DROVE THEM OFF")
+                            #GOTO 2395
+                        else:
+                            if not (B1 <= 4):
+                                print("LOUSY SHOT---YOU GOT KNIFED")
+                                K8 = 1
+                                print("YOU HAVE TO SEE OL' DOC BLANCHARD")
+                                #GOTO 2395
+                            else:
+                                print("KINDA SLOW WITH YOUR COLT .45")
+                                #GOTO 2395
+                    else: #2285
+                        if not(T1 > 3): #2310
+                            if not (random.random() > 0.8): #2390
+                                B = B - 150
+                                M1 = M1 -15
+                                #GOTO 2395
+                        else: 
+                            #GOSUB 4500 TODO
+                            B = B - B1 * 30 - 80
+                            M1 = M1 -15
+                            #GOTO 2235
+    
+"""Presents the event of player dying. """    
+def dying():
+    print("\n")
+    print("DO TO YOUR UNFORTUNATE SITUATION, THERE ARE A FEW")
+    print("FORMALITIES WE MUST GO THROUGH")
+    print("\n")
+    print("WOULD YOU LIKE A MINISTER?")
+    CR = raw_input()
+    print("WOULD YOU LIKE A FANCY FUNERAL?")
+    CR = raw_input()
+    print("WOULD YOU LIKE US TO INFORM YOUR NEXT OF KIN?")
+    CR = raw_input()
+    if not(CR == "YES"):
+        print("YOUR AUNT NELLIE IN ST. LOUIS IS ANXIOUS TO HEAR")
+        print("\n")
+    print("WE THANK YOU FOR THIS INFORMATION AND WE ARE SORRY YOU")
+    print("DIDN'T MAKE IT TO THE GREAT TERRITORY OF OREGON")
+    print("BETTER LUCK NEXT TIME")
+    print("\n\n")
+    print(get_tab(30) + "SINCERELY")
+    print("\n")
+    print(get_tab(17) + "THE OREGON CITY CHAMBER OF COMMERCE")
+    
+"""Given an integer n, the number of tabs, returns a string with the number of 
+n tabs."""    
+def get_tab(n):
+    string = ""
+    for i in xrange(n):
+        string += "\t"
+    return string
+
 if __name__ == "__main__" :
     print ("DO YOU NEED INSTRUCTIONS(YES/NO)?")
     user_input = raw_input()
